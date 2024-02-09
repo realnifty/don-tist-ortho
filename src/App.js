0bfc979ab2a60ef95e802e7d51b0d76f35d7452b
space cowboy <rogelioalbertopedraza@gmail.com>
2024-02-09 15:04:01 -0600

    navbar styled, font imports, btsrp icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import './style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './pages/Home';
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,19 +1,20 @@
-import Braces from '../../images/brace.png'
+import Braces from '../../images/brace.png';
 
 const Navbar = () => {
 	return (
-    <div>
-      <div>
-        <img src={Braces} alt='logo' className='brace-logo'/>
-        <div>
-          <h1>Don Tist</h1>
-          <p>Orthodontics</p>
-        </div>
-      </div>
-			<nav className='navbar navbar-expand-lg navbar-light bg-light d-none'>
-				<a className='navbar-brand' href='#'>
-					Navbar
-				</a>
+		<div>
+			<nav className='navbar d-flex flex-row align-items-center justify-content-between p-3'>
+					<div className='d-flex flex-row'>
+						<img src={Braces} alt='logo' className='brace-logo me-2' />
+						<div className='d-flex flex-column'>
+							<h1 className='m-0 notable'>Don Tist</h1>
+							<p className='m-0 text-center fs-5 raleway'>Orthodontics</p>
+						</div>
+					</div>
+					<i className='bi bi-telephone-fill fs-1'></i>
+					<button className='navbar-toggler'>
+						<span className='navbar-toggler-icon'></span>
+					</button>
 			</nav>
 		</div>
 	);
