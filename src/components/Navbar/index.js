2feab8076f8590210f0644dc2a01794d0d8d1f8b
space cowboy <rogelioalbertopedraza@gmail.com>
2024-02-11 15:17:03 -0600

    navbar dropwdown simple functionality

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,47 @@
+import { useState } from 'react';
+
 import Braces from '../../images/brace.png';
 
 const Navbar = () => {
+	const [navbarOpen, setNavbarOpen] = useState(false);
+
+	const toggleNavbarOpen = () => {
+		setNavbarOpen(!navbarOpen);
+	};
+
+	const handleBlur = () => {
+		setNavbarOpen(false);
+	};
+
 	return (
 		<div>
-			<nav className='navbar d-flex flex-row align-items-center justify-content-between p-3'>
-					<div className='d-flex flex-row'>
-						<img src={Braces} alt='logo' className='brace-logo me-2' />
-						<div className='d-flex flex-column'>
-							<h1 className='m-0 notable'>Don Tist</h1>
-							<p className='m-0 text-center fs-5 raleway'>Orthodontics</p>
-						</div>
+			<nav className='navbar d-flex flex-row align-items-center justify-content-between p-3 text-black'>
+				<div className='d-flex flex-row align-items-center'>
+					<img src={Braces} alt='logo' className='brace-logo me-2' />
+					<div className='d-flex flex-column'>
+						<h1 className='m-0 notable name'>Don Tist</h1>
+						<p className='m-0 text-center fs-5 raleway occupation'>
+							Orthodontics
+						</p>
 					</div>
-					<i className='bi bi-telephone-fill fs-1'></i>
-					<button className='navbar-toggler'>
-						<span className='navbar-toggler-icon'></span>
-					</button>
+				</div>
+				<i className='bi bi-telephone-fill fs-1 name'></i>
+				<button
+					className={`navbar-toggler ${navbarOpen ? `` : `collapsed`}`}
+					onClick={toggleNavbarOpen}
+					onBlur={handleBlur}
+				>
+					<span className='navbar-toggler-icon name'></span>
+				</button>
+				<div
+					className={`bg-danger navbar-collapse ${navbarOpen ? `open` : `collapse`}`}
+				>
+					<ul className='navbar-nav'>
+						<li className='nav-item'>hello</li>
+						<li className='nav-item'>hello</li>
+						<li className='nav-item'>hello</li>
+					</ul>
+				</div>
 			</nav>
 		</div>
 	);
